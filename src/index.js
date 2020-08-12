f9be2887675fb62d2a238f9ff4df828131f974a7
David Elì <davidelimg1@gmail.com>
2020-08-11 19:21:56 -0500

    6- Redux => Store methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import './examples/5counterReducer'
+import './examples/6storeMethods'
 
-const App = () => {
-  return <h1>Hello World!</h1>;
-};
+// const App = () => {
+//   return <h1>Hello World!</h1>;
+// };
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

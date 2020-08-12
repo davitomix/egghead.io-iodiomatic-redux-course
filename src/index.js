dc29e5c862d9a66159dbee336361f20f24de5c7f
David Elì <davidelimg1@gmail.com>
2020-08-11 19:05:57 -0500

    5- counter Reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import './examples/11todoListReducer'
+import './examples/5counterReducer'
 
-// const App = () => {
-//   return <h1>Hello</h1>;
-// };
+const App = () => {
+  return <h1>Hello World!</h1>;
+};
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

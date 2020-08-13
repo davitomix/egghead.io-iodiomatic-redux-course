15a84902877cb2b818aaead22295e7e2cd46eb93
David Elì <davidelimg1@gmail.com>
2020-08-13 15:27:37 -0500

    10 - avoiding object mutations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import './examples/9avoidingArrayMutations'
+import './examples/10avoidingObjectMutations'
 
 // const App = () => {
 //   return <h1>Hello World!</h1>;

8bff03519f333f7ee829f602464c6676bf671145
David Elì <davidelimg1@gmail.com>
2020-08-13 15:16:59 -0500

    9- Avoiding array muttations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import './examples/8reactCounterExmple'
+import './examples/9avoidingArrayMutations'
 
 // const App = () => {
 //   return <h1>Hello World!</h1>;

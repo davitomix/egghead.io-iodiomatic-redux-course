d53c7744967b397765b8ea0352a8e4215b436cd6
David Elì <davidelimg1@gmail.com>
2020-08-19 12:40:02 -0500

    22 - extracting Container Component Filter Link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import './examples/21extractinPresentationalComponents'
+import './examples/22extractingContainerCompontentFilterLink'
 
 // const App = () => {
 //   return <h1>Hello World!</h1>;

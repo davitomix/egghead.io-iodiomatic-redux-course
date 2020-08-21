57c5aff54cff526b10f8950278fada97e2f02af4
David Elì <davidelimg1@gmail.com>
2020-08-21 16:20:40 -0500

    10 - Colocating Selectors with reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,18 +1,18 @@
 const todo = (state, action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
+  switch (action.type) {
+    case "ADD_TODO":
       return {
         id: action.id,
         text: action.text,
-        completed: false
+        completed: false,
       };
-    case 'TOGGLE_TODO':
-      if(state.id !== action.id) {
+    case "TOGGLE_TODO":
+      if (state.id !== action.id) {
         return state;
       }
       return {
         ...state,
-        completed: !state.completed
+        completed: !state.completed,
       };
     default:
       return state;
@@ -21,16 +21,26 @@ const todo = (state, action) => {
 
 export const todos = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
+    case "ADD_TODO":
+      return [...state, todo(undefined, action)];
+    case "TOGGLE_TODO":
+      return state.map((t) => todo(t, action));
     default:
       return state;
   }
 };
 
 export default todos;
+
+export const getVisibleTodos = (state, filter) => {
+  switch (filter) {
+    case "all":
+      return state;
+    case "completed":
+      return state.filter((t) => t.completed);
+    case "active":
+      return state.filter((t) => !t.completed);
+    default:
+      throw new Error(`Unknown filter: ${filter}.`);
+  }
+};
